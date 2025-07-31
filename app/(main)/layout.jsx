274c4c0afcb8e9aeaa5fde403c1c0563442391f0
Majed Hasan <mdmajedhasan599@gmail.com>
2025-07-31 22:39:57 +0600

    For now hide the header and footer

--- app/(main)/layout.jsx
+++ app/(main)/layout.jsx
@@ -1,13 +1,9 @@
 import React from "react";
-import Header from "../_components/common/Header.jsx";
-import Footer from "../_components/common/Footer.jsx";
 
 const layout = ({ children }) => {
   return (
     <>
-      <Header />
       <main>{children}</main>
-      <Footer />
     </>
   );
 };
