abaef4ebd2d24f0f1d55337de283cca17c780dfc
Majed Hasan <mdmajedhasan599@gmail.com>
2025-07-31 23:35:46 +0600

    Change to lowercase

--- app/(main)/layout.jsx
+++ app/(main)/layout.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Header from "@/app/_components/common/Header";
+import Header from "@/app/_components/common/header";
 
 const layout = ({ children }) => {
   return (
