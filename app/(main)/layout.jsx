f8bee69e1178fd180e82804491d21ece1490cc3a
Majed Hasan <mdmajedhasan599@gmail.com>
2025-07-31 22:44:24 +0600

    Header Added

--- app/(main)/layout.jsx
+++ app/(main)/layout.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import Header from "../_components/common/Header";
 
 const layout = ({ children }) => {
   return (
     <>
-      <main>{children}</main>
+      <main>
+        <Header />
+        {children}
+      </main>
     </>
   );
 };
